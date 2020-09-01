862849e78652af1b812f2b86a4c76a028b73f8ef
jenxime <jenifferximena1996@hotmail.com>
2020-08-31 20:02:22 -0500

    Parte logica de presentacion 1.0

---
+++ src/java/BackingBean/JSFMB_FindFastPro.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BackingBean;
+
+import Entidades.Cliente;
+import Sesiones.ClienteFacadeLocal;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Xime
+ */
+@ManagedBean(name = "JSFMB_FindFastPro")
+//@RequestScoped
+@SessionScoped
+public class JSFMB_FindFastPro implements Serializable{
+     //las variables 
+
+    @EJB
+    private ClienteFacadeLocal manejadorCliente;
+    private List<Cliente> ListaClientes;
+    private Cliente cliente;
+    
+    public JSFMB_FindFastPro() {
+    }
+    
+    //CODIGO PARA AGREGAR A UN CLIENTE
+
+    public void grabarCliente() {
+        manejadorCliente.create(cliente);
+        this.ListaClientes();
+        //this.recuperarCliente();
+    }
+
+    public void ListaClientes() {
+        setListaClientes(manejadorCliente.findAll());
+      
+    }
+    
+    @PostConstruct
+    private void inicio() {
+        cliente = new Cliente();
+        ListaClientes();
+        
+    }
+
+    public ClienteFacadeLocal getManejadorCliente() {
+        return manejadorCliente;
+    }
+
+    public void setManejadorCliente(ClienteFacadeLocal manejadorCliente) {
+        this.manejadorCliente = manejadorCliente;
+    }
+
+    public List<Cliente> getListaClientes() {
+        return ListaClientes;
+    }
+
+    public void setListaClientes(List<Cliente> ListaClientes) {
+        this.ListaClientes = ListaClientes;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+    
+    
+}
